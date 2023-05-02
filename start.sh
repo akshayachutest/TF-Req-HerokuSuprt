e22f6a90173a51692599c4653fdb3add9945e8b9
akshayachutest <132114730+akshayachutest@users.noreply.github.com>
2023-05-02 20:38:45 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,8 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/FarzanGit/charlie /charlie
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /charlie
 fi
 cd /charlie
 pip3 install -U -r requirements.txt
